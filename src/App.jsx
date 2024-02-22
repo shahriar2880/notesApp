16fae2d9f31388fc6cddfe3b64be4bb08da078fa
shahriar2880 <shahriarreza436@gmail.com>
2024-02-22 18:50:11 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <div className="main">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
